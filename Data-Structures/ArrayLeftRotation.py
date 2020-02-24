ee1300075ce8e8bc8b2a6c4762931f6836cac22a
MsMansiDhruv <38699181+MsMansiDhruv@users.noreply.github.com>
2020-02-24 15:23:01 +0530

    Create ArrayLeftRotation.py
    https://www.hackerrank.com/challenges/array-left-rotation/problem?h_r=next-challenge&h_v=zen
    
    Useful Link:
    https://www.geeksforgeeks.org/print-lists-in-python-4-different-ways/
    https://www.geeksforgeeks.org/python-ways-to-rotate-a-list/

---
+++ Data-Structures/ArrayLeftRotation.py
@@ -0,0 +1,20 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    test_list = a[d:] + a[:d] 
+    
+    print(*test_list)  
