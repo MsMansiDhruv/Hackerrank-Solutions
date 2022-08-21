b12eb29ce2481d40e2cdd9d72adc4f78a3b35116
MsMansiDhruv <38699181+MsMansiDhruv@users.noreply.github.com>
2022-08-21 14:33:32 +0530

    Update ArrayManipulation.py

--- Data-Structures/Array/ArrayManipulation.py
+++ Data-Structures/Array/ArrayManipulation.py
@@ -10,19 +10,35 @@ import sys
 
 #Code modified for O(n)
 def arrayManipulation(n, queries):
-    arr = [0]*(n+1)
-
-    for x, y, incr in queries:
-        arr[x-1] += incr
-        if y <= len(arr):
-            arr[y] -= incr
-    m=x=0
-
+    #Note: The indexing in example starts from 1. The a and b values are inclusive
+    
+    #Method 1: Brute force: Using 2+1 for loops. Complexity: O(n^q), q=length of queries, n=input
+    """
+    arr = [0]*n
+    maxVal = 0
+    for a,b,k in queries:
+        for i in range(a-1,b):
+            arr[i]+=int(k)
+    for val in arr:
+        if maxVal < val: maxVal = val
+    return maxVal   
+    """
+    
+    #Method 2: Using prefix Sum algorithm. Complexity: O(q) + O(n), q=length of queries, n=input 
+    arr = [0]*(n+1) #Take one extra element for easy indexing.
+    maxVal = 0
+    x = 0
+    for a,b,k in queries:
+        arr[a-1] += k
+        if b <= len(arr):
+            arr[b] -= k
+            
     for i in arr:
-        x += i
-        if m < x:
-            m = x
-    return m
+        x+=i
+        if x>maxVal: maxVal=x
+        
+    return maxVal
+        
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
