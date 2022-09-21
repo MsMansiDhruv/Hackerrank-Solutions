1ad0f184269260837bbb4f0f552e31c0e07054d6
MsMansiDhruv <38699181+MsMansiDhruv@users.noreply.github.com>
2022-09-22 00:02:01 +0530

    Update LargestRectangle.py
    https://www.hackerrank.com/challenges/largest-rectangle/problem?isFullScreen=true

--- Data-Structures/Stacks/LargestRectangle.py
+++ Data-Structures/Stacks/LargestRectangle.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import math
 import os
 import random
@@ -6,36 +8,49 @@ import sys
 from collections import deque
 
 # Complete the largestRectangle function below.
+
+#Time Complexity: O(n), Space Complexity: O(n)
+def previousSmaller(h):
+    indexI = []
+    indexJ = []
+    val = []
+    for i in range(0,len(h)):
+        while(len(val)>0 and val[-1]>=h[i]):
+            val.pop()
+            indexJ.pop()
+
+        if len(val)==0:
+            indexI.append(-1)
+        else:
+            indexI.append(indexJ[-1])
+        indexJ.append(i)
+        val.append(h[i])  
+    return indexI
+    
+def nextSmaller(h):
+    indexI = []
+    indexJ = []
+    val = []
+    n = len(h)
+    for i in range(n-1, -1, -1):
+        while(len(val)>0 and val[-1]>=h[i]):
+            val.pop(-1)
+            indexJ.pop(-1)
+        if len(val)==0:
+            indexI.append(n)
+        else:
+            indexI.append(indexJ[-1])
+        indexJ.append(i)
+        val.append(h[i])
+    return indexI[::-1]        
+    
 def largestRectangle(h):
-    pos = deque()
-    height = deque()
-    max_area = 0
-    iterator = 0
-    
-    while (iterator < len(h) ):
-        if len(height)==0 or len(pos) == 0 or h[iterator] >= height[-1]:
-            height.append(h[iterator])
-            pos.append(iterator)
-        elif len(height) > 0 and len(pos) > 0 and h[iterator] < height[-1]:
-            while len(height) > 0 and len(pos) > 0 and h[iterator] < height[-1]: 
-                element = height.pop()
-                index = pos.pop()
-                print(index)
-                no_of_buildings = iterator - index
-                area = element * no_of_buildings
-                max_area = max(area, max_area)
-                # print(height, pos)
-            height.append(h[iterator])
-            pos.append(index)
-        iterator += 1
-    while len(height) > 0:
-        element = height.popleft()
-        index = pos.popleft()
-        area = (len(h) - index) * element
-        max_area = max(area, max_area)    
-    return max_area
-                        
-    
+    l = previousSmaller(h)
+    m = nextSmaller(h)
+    area = [e2-e1-1 for e1,e2 in zip(l,m) ]
+    for i in range(len(h)):
+        area[i] = area[i]*h[i]
+    return max(area)
     
                 
 if __name__ == '__main__':
