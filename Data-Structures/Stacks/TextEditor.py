49555529a41e9c97b14ffb25256d8377a472f27b
MsMansiDhruv <38699181+MsMansiDhruv@users.noreply.github.com>
2022-09-22 23:58:38 +0530

    Update TextEditor.py

--- Data-Structures/Stacks/TextEditor.py
+++ Data-Structures/Stacks/TextEditor.py
@@ -1,20 +1,22 @@
 from collections import deque
-
 S = ""
-previousState = deque()
-for _ in range(int(input())):
+undoS = deque()
+n = int(input())
+
+for i in range(n):
     line = input().split()
-    # print(line)
-    if line[0] == '1':
-        previousState.append(S)
-        S += line[1]
-        # print(previousState)
-    elif line[0] == '2':
-        previousState.append(S)
-        S = S[:-int(line[1])]   
-    elif line[0] == '3':
-        print(S[int(line[1])-1])
-    elif line[0] == '4':
-        S = previousState.pop()
-        # print(S)
     
+    if line[0]=='1':
+        undoS.append(S)
+        S += line[1]
+    elif line[0]=='2':
+        undoS.append(S)
+        m = int(line[1])
+        S = S[:-m]
+    elif line[0]=='3':
+        m = int(line[1])-1
+        if m < len(S):
+            print(S[m])   
+    elif line[0]=='4':
+        S = undoS.pop()
+            
